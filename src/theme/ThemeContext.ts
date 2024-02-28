b90148bbb4d10135310637dacf047a3a61d07b7a
Irina Litvinova <neforsha@yandex.ru>
2024-02-28 14:33:02 +0700

    feat: project initialization

---
+++ src/theme/ThemeContext.ts
@@ -0,0 +1,15 @@
+import { createContext } from "react";
+
+export enum Theme {
+  LIGHT = "light",
+  DARK = "dark",
+}
+
+export interface ThemeContextPops {
+  theme?: Theme;
+  setTheme?: (theme: Theme) => void;
+}
+
+export const ThemeContext = createContext<ThemeContextPops>({});
+
+export const LOCAL_STORAGE_THEME_KEY = "theme";
