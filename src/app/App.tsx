d78a1dfd57886c6996f7d3780bec60a172cadab3
Irina Litvinova <neforsha@yandex.ru>
2024-02-28 17:03:33 +0700

    feat: added Navbar and Styled Links

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,17 +3,16 @@ import "./styles/index.scss";
 import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "./providers/ThemeProvider";
 import { AppRouter } from "./providers/router";
-
+import { Navbar } from "widgets/Navbar";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className={classNames('app', {}, [theme])}>
+    <div className={classNames("app", {}, [theme])}>
+      <Navbar />
+      <AppRouter />
       <button onClick={toggleTheme}>TOGGLE</button>
-      <Link to={"/"}>Главная</Link>
-      <Link to={"/about"}>О сайте</Link>
-      <AppRouter/>
     </div>
   );
 };
