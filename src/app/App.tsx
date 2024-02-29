61ca126741a36987033b3425fd3cebae6f73e35a
Irina Litvinova <neforsha@yandex.ru>
2024-02-29 18:21:34 +0700

    feat: added jest and the first unit test

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,29 +1,25 @@
-import { Link, Route, Routes } from "react-router-dom";
-import "./styles/index.scss";
-import { classNames } from "shared/lib/classNames/classNames";
-import { useTheme } from "./providers/ThemeProvider";
-import { AppRouter } from "./providers/router";
-import { Navbar } from "widgets/Navbar";
-import { ThemeSwitcher } from "widgets/ThemeSwitcher";
-import { Sidebar } from "widgets/Sidebar";
-import { Suspense } from "react";
-
-
+import './styles/index.scss';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Navbar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/Sidebar';
+import { Suspense } from 'react';
+import { AppRouter } from './providers/router';
+import { useTheme } from './providers/ThemeProvider';
 
 const App = () => {
-  const { theme } = useTheme();
+    const { theme } = useTheme();
 
-  return (
-    <div className={classNames("app", {}, [theme])}>
-      <Suspense fallback="">
-        <Navbar />
-        <div className="content-page">
-          <Sidebar />
-          <AppRouter />
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <Suspense fallback="">
+                <Navbar />
+                <div className="content-page">
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
         </div>
-      </Suspense>
-    </div>
-  );
+    );
 };
 
 export default App;
---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,30 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+    test('with one param', () => {
+        expect(classNames('someClass')).toBe('someClass');
+    });
+
+    test('with add class', () => {
+        const expectation = 'someClass class1 class2';
+        expect(classNames('someClass', {}, ['class1', 'class2'])).toBe(expectation);
+    });
+
+    test('with mods', () => {
+        const expectation = 'someClass class1 class2 hovered scrollable';
+        expect(classNames(
+            'someClass',
+            { hovered: true, scrollable: true },
+            ['class1', 'class2'],
+        )).toBe(expectation);
+    });
+
+    test('without scrolling', () => {
+        const expectation = 'someClass class1 class2 hovered';
+        expect(classNames(
+            'someClass',
+            { scrollable: undefined, hovered: true },
+            ['class1', 'class2'],
+        )).toBe(expectation);
+    });
+});
