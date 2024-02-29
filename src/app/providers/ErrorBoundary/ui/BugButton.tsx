c6c72e3ed6b1e8e31a1823ac1b4486b3de0be96f
Irina Litvinova <neforsha@yandex.ru>
2024-02-29 22:50:31 +0700

    feat: added ErrorBoundary

---
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -0,0 +1,24 @@
+import { Button } from 'shared/ui/Button/Button';
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+// компонент для тестирования ErrorBoundary
+export const BugButton = () => {
+    const [error, setError] = useState(false);
+    const { t } = useTranslation();
+    const throwHandler = () => setError(true);
+
+    useEffect(() => {
+        if (error) {
+            throw new Error();
+        }
+    }, [error]);
+
+    return (
+        <Button
+            onClick={throwHandler}
+        >
+            {t('throw error')}
+        </Button>
+    );
+};
