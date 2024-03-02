99406468a8a4f1728a11240b4977920b4cb35b99
Irina Litvinova <neforsha@yandex.ru>
2024-03-02 16:57:59 +0700

    fix: fixed Sidebar`s imports

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,8 +3,8 @@ import React, { useState } from 'react';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { LangSwitcher } from 'widgets/LangSwitcher/ui/LangSwitcher';
 import { Button } from 'shared/ui/Button/Button';
-import cls from './Sidebar.module.scss';
 import { useTranslation } from 'react-i18next';
+import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
   className?: string;
