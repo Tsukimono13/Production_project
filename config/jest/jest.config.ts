bbb9e3d3c7b3debfccc13725a6fe08956b8635f5
Irina Litvinova <neforsha@yandex.ru>
2024-04-08 17:29:19 +0700

    fix: fixed jest config and theme for storybook

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -41,6 +41,7 @@ const config: InitialOptions = {
     moduleNameMapper: {
         '\\.s?css$': 'identity-obj-proxy',
         '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
+        '^@/(.*)$': '<rootDir>/src/$1',
     },
     reporters: [
         'default',
