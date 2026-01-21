a67d301fc2aff35aefa261ec9f2dfe66e45a836c
Tsukimono <tsukimono@MacBook-Air-Tsukimono.local>
2026-01-21 16:38:34 +0300

    fix some errors

--- src/features/articlePageGreeting/ui/ArticlePageGreeting/ArticlePageGreeting.stories.tsx
+++ src/features/articlePageGreeting/ui/ArticlePageGreeting/ArticlePageGreeting.stories.tsx
@@ -11,7 +11,7 @@ export default {
     },
 } as ComponentMeta<typeof ArticlePageGreeting>;
 
-const Template: ComponentStory<typeof ArticlePageGreeting> = (args) => <ArticlePageGreeting {...args} />;
+const Template: ComponentStory<typeof ArticlePageGreeting> = () => <ArticlePageGreeting />;
 
 export const Normal = Template.bind({});
 Normal.args = {
